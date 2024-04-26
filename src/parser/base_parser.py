d38d6e02fd3b458d45b9ab9f143ddd911383eb9b
pom <sudoPom@github.com>
2024-04-26 14:02:11 +0100

    Remove last print statement

--- src/parser/base_parser.py
+++ src/parser/base_parser.py
@@ -5,7 +5,6 @@ class BaseParser:
     def __init__(self, start_from: str, grammar: str):
         self.__start_from = start_from
         self.__grammar = grammar
-        print(grammar)
 
     def parse(self, input_string: str):
         parser = Lark(self.__grammar, start=self.__start_from)
